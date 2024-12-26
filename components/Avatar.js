e468787de253703d64cca82f2546e7e95c8183c0
Imad Laichi <141133660+imadlaichi@users.noreply.github.com>
2024-12-26 14:11:50 +0100

    Update Avatar.js

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Avatar = () => {
   return <div className="hidden xl:flex xl:max-w-none">
-    <Image src={"/logo-data-xplore.png"} width={437} height={478} alt="" className="translate-z- w-full h-full "/>
+    <Image src={"/logo-main.png"} width={437} height={478} alt="" className="translate-z- w-full h-full "/>
   </div>;
 };
 
